2a6832c965265befcceea0690ecd02cd4c81f302
gutenachtgeschichten <jheckert83@gmail.com>
2026-07-08 10:51:03 +0200

    Update stories.js

--- stories.js
+++ stories.js
@@ -1,9 +1,9 @@
 const stories = [
   {
-    label: "Geschichte 1",
-    title: "Der kleine Fuchs",
-    audio: "audio/geschichte-1.mp3"
-  },
+  label: "Geschichte 1",
+  title: "Pass auf dich auf, kleine Maus",
+  audio: "audio/PassaufdichaufkleineMaus.m4a"
+},
   {
     label: "Geschichte 2",
     title: "Der Mond über Stuttgart",
